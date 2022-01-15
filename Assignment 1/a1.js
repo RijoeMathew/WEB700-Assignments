bd2052143024d92ed644838ba99a9825c4e3a4fc
Rijoe Mathew <rijoe.c.mathew@gmail.com>
2022-01-15 16:50:39 -0500

    Add files via upload
    Completed till step 3

--- Assignment 1/a1.js
+++ Assignment 1/a1.js
@@ -1,1 +1,3 @@
-console.log("Hello World");
+var serverVerbs = ["GET","GET","GET","POST","GET","POST"]
+var serverPaths = ["/","/about","/contact","/login","/panel","/logout"]
+var serverResponses = ["Welcome to WEB700 Assignment 1","This assignment was prepared by Rijoe Chacko Mathew","Rijoe Chacko Mathew: rcmathew1@myseneca.ca","User Logged In","Main Panel","Logout Complete"]
