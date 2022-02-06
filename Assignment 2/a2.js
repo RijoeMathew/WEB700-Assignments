f421d559c381e05cea50624b3b763cb4e1b17469
Rijoe Mathew <rijoe.c.mathew@gmail.com>
2022-02-05 21:40:44 -0500

    Final completed code with commenting

--- Assignment 2/a2.js
+++ Assignment 2/a2.js
@@ -4,15 +4,15 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Rijoe Chacko Mathew Student ID: 132469214 Date: 01/30/2022
+*  Name: Rijoe Chacko Mathew Student ID: 132469214 Date: 02/05/2022
 *
 ********************************************************************************/
 
-var collegeData = require("./modules/collegeData.js");
+var collegeData = require("./modules/collegeData.js"); //Importing the collegeData Module
 
-
-collegeData.initialize().then(success => {
+//if initialize function is resolved, we are checking for other functions and if any of the promises are rejected same is being handled
+collegeData.initialize().then(() => {
     collegeData.getAllStudents().then(success => {console.log(`Successfully retrieved ${success.length} students`)}).catch(error => {console.log('FAILED: ' + error)});
     collegeData.getCourses().then(success => {console.log(`Successfully retrieved ${success.length} courses`)}).catch(error => {console.log('FAILED: ' + error)});
     collegeData.getTAs().then(success => {console.log(`Successfully retrieved ${success.length} TAs`)}).catch(error => {console.log('FAILED: ' + error)});
-}).catch(error => {console.log('FAILED: ' + error)});
+}).catch(error => {console.log('FAILED: ' + error)}); 
--- Assignment 2/modules/collegeData.js
+++ Assignment 2/modules/collegeData.js
@@ -1,3 +1,4 @@
+//create data class with two properties students and courses
 class Data{
     constructor(students,courses){
         this.students=students;
@@ -5,98 +6,108 @@ class Data{
     }
 }
 
-var dataCollection=null;
-var fs = require('fs');
 
+var dataCollection=null; //initialise variable that holds an object of Data class later
+var fs = require('fs'); //import the fs module
+
+//initialise function to read the students.json and courses.js using promise
 module.exports.initialize = function(){
-return new Promise(function(resolve, reject){    
+return new Promise(function(resolve, reject){        
     
+    var studentDataFromFile = null; //initialise studentDataFromFile variable as null
+    var courseDataFromFile = null; //initialise courseDataFromFile variable as null
     
-    var studentDataFromFile = null;
-    var courseDataFromFile = null;    
-    
+    //creating studentPromise object to read students.json file
     const studentPromise = new Promise(function(resolve,reject){    
-    fs.readFile('./data/students.json', 'utf8', function(err, data){
-        
-        if (err){
-            console.log('#4'); 
-            reject("unable to read students.json")
-            return; // exit the function
-        }
-        
-        studentDataFromFile = JSON.parse(data); // convert the JSON from the file into an array of objects 
-        resolve('Read students.json file');
-    });   
+        fs.readFile('./data/students.json', 'utf8', function(err, data){
+            
+            if (err){
+                reject("unable to read students.json"); //reject the promise since there was an error while reading the students.json file
+                return; // exit the function
+            }
+            
+            studentDataFromFile = JSON.parse(data); // convert the JSON from the file into an array of objects 
+            resolve('Read students.json file'); //resolve the promise since reading students.json is completed
+            
+        });   
 
     });
 
-    
+    //creating studentPromise object to read courses.json file
     const coursePromise = new Promise(function(resolve,reject){
-    fs.readFile('./data/courses.json', 'utf8', function(err, data){
-        
-        if (err){
-            reject("unable to read courses.json")
-            return; // exit the function
-        }
-        
-        courseDataFromFile = JSON.parse(data); // convert the JSON from the file into an array of objects  
-        resolve('Read courses.json file');       
-    });
+        fs.readFile('./data/courses.json', 'utf8', function(err, data){
+            
+            if (err){     
+                reject("unable to read courses.json");//reject the promise since there was an error while reading the courses.json file
+                
+                return; // exit the function
+            }
+             
+            courseDataFromFile = JSON.parse(data); // convert the JSON from the file into an array of objects  
+            resolve('Read courses.json file');  //resolve the promise since reading courses.json is completed     
+        });
 
-    });
-
-    studentPromise.then(success => {coursePromise.then(success => 
-        {   
-            dataCollection = new Data(studentDataFromFile, courseDataFromFile);
-            resolve('initialize() successfully completed'); 
-        }).catch(error => {console.log('FAILED: ' + error)})
-    }).catch(error => {console.log('FAILED: ' + error)});
+    });    
+       
+    studentPromise.then(success => {
+        coursePromise.then(success => 
+        {
+            dataCollection = new Data(studentDataFromFile, courseDataFromFile);            
+            resolve('initialize() successfully completed');
+        }).catch(error => {reject(error)})
+    }).catch(error => {reject(error)});
+    
 });
 
 };
 
 
+
+//creating Promise object for getallstudents function to get all the student objects
 module.exports.getAllStudents = function(){
 return new Promise(function(resolve, reject){
-    if (dataCollection.students.length != 0){
-        resolve(dataCollection.students);
+    if (dataCollection.students.length != 0){ //checks if student objects array is empty or not
+        resolve(dataCollection.students); //resolve the promise since student objects array is not empty
     }
     else{
-        reject("no results returned");
+        reject("no results returned"); //reject the promise since student objects array is empty
     }    
 });
 };
 
 
+
+//creating Promise object for getTas function to get all the student objects of students who are TAs
 module.exports.getTAs = function(){
 return new Promise(function(resolve, reject){
-    var studentsTAs= [];
+    var studentsTAs= []; //initialise studentsTA array
 
     for(i=0;i<dataCollection.students.length;i++) //iterates over 0 till the last index of dataCollection.students array
     {       
-        if (dataCollection.students[i].TA){
-            studentsTAs.push(dataCollection.students[i]);
+        if (dataCollection.students[i].TA){ //checks if the TA property of the particular student object is true or not
+            studentsTAs.push(dataCollection.students[i]); //add the student object to the studentTsA array
         }
     }
 
-    if (studentsTAs.length != 0){
-        resolve(studentsTAs);        
+    if (studentsTAs.length != 0){ //checks if studentTA array is empty or not
+        resolve(studentsTAs);   //resolve the promise since the studentsTA array is not empty   
     }
     else{
-        reject("no results returned");
+        reject("no results returned"); //reject the promise since the studentsTA array is empty 
     }      
 });
 };
 
 
 
+//creating Promise object for getCourses function to get all the course objects
 module.exports.getCourses = function(){
 return new Promise(function(resolve, reject){
-    if (dataCollection.courses.length != 0){
-        resolve(dataCollection.courses);
+    if (dataCollection.courses.length != 0){ //checks if courses objects array is empty or not
+        resolve(dataCollection.courses);  //resolve the promise since courses objects array is not empty
     }
     else{
-        reject("no results returned");
+        reject("no results returned"); //reject the promise since courses objects array is empty
     }    
 })
 };
