8e5f6f44943120ec1543efe9ad691526d0eda63e
Rijoe Mathew <rijoe.c.mathew@gmail.com>
2022-02-15 16:11:25 -0500

    Completed all steps and tested the website

--- Assignment 3/web700-app/server.js
+++ Assignment 3/web700-app/server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Rijoe Chacko Mathew Student ID: 132469214 Date: 19-FEB-2022
+*
+********************************************************************************/
+
+
 var collegeData = require("./modules/collegeData.js"); //Importing the collegeData Module
 var path = require("path"); //Importing the path Module
 
