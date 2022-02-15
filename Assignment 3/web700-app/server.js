611941fe27986db9d5c776a365dcf395eca4bdca
Rijoe Mathew <rijoe.c.mathew@gmail.com>
2022-02-15 18:26:16 -0500

    Final commented code

--- Assignment 3/web700-app/server.js
+++ Assignment 3/web700-app/server.js
@@ -16,61 +16,69 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
-
+// setup a 'route' to listen on the url path /students and additionaly to /students?course=value
 app.get("/students", (req, res) => {
 
-    if (Object.keys(req.query).length == 0){    
-    collegeData.getAllStudents().then(success => {res.send(JSON.stringify(success))})
-        .catch(() => {res.send(JSON.stringify({message:"no results"}) )})  
+    
+    if (Object.keys(req.query).length == 0){    //checks if the req.query object is empty 
+    collegeData.getAllStudents().then(success => {res.send(JSON.stringify(success))})  //use the getAllStudents to get all student objects and sends the json formatted string if successfull
+        .catch(() => {res.send(JSON.stringify({message:"no results"}) )})  //when the method invokes reject, send the mentioned object back as json
     }
 
+    //use the getStudentsByCourse to get all student objects with course parameter having value as req.query.course and sends the json formatted string if successfull
     else{  
     collegeData.getStudentsByCourse(req.query.course).then(success => {res.send(JSON.stringify(success))})
-        .catch(() => {res.send(JSON.stringify({message:"no results"}) )})     
+        .catch(() => {res.send(JSON.stringify({message:"no results"}) )})    //when the method invokes reject, send the mentioned object back as json
     }  
     });
 
 
+// setup a 'route' to listen on the url path /tas and sends the json formatted string of all students who are TAs
 app.get("/tas", (req, res) => {        
     collegeData.getTAs().then(success => {res.send(JSON.stringify(success))})
         .catch(() => {res.send(JSON.stringify({message:"no results"}) )})
     });
 
 
+// setup a 'route' to listen on the url path /courses and sends the json formatted string of all courses
 app.get("/courses", (req, res) => {        
     collegeData.getCourses().then(success => {res.send(JSON.stringify(success))})
         .catch(() => {res.send(JSON.stringify({message:"no results"}) )})
     });
 
 
+// setup a 'route' to listen on the url path /student/:num and sends the json object of the student whose studentNum parameter holds the value same as num
 app.get("/student/:num", (req, res) => {     
     collegeData.getStudentByNum(req.params.num).then(success => {res.send(JSON.stringify(success))})
         .catch(() => {res.send(JSON.stringify({message:"no results"}) )})
     });
 
-
-// setup a 'route' to listen on the default url path    
-app.get("/", (req, res) => {     
-    res.sendFile(__dirname +'/views/home.html')
-    });
-
  
+// setup a 'route' to listen on the default url path 
+app.get("/", (req, res) => {     
+    res.sendFile(path.join(__dirname +'/views/home.html'))
+    });
+
+
+// setup a 'route' to listen on the url path /about
 app.get("/about", (req, res) => {     
-    res.sendFile(__dirname +'/views/about.html')
+    res.sendFile(path.join(__dirname +'/views/about.html'))
     });
 
 
+// setup a 'route' to listen on the url path /htmlDemo
 app.get("/htmlDemo", (req, res) => {     
-    res.sendFile(__dirname +'/views/htmlDemo.html')
+    res.sendFile(path.join(__dirname +'/views/htmlDemo.html'))
     });
 
 
+// used to add middleware to return page not found message when the user enters a route that is not matched with anything in the app 
 app.use((req, res) => {
     res.status(404).send("Page Not Found");
     });
 
 
-// setup http server to listen on HTTP_PORT
+// checks if initialize method is successfull then setup http server to listen on HTTP_PORT, if initalize method invoked reject error is displayed on console
 collegeData.initialize().then(() => {
     app.listen(HTTP_PORT, ()=>{console.log("server listening on port: " + HTTP_PORT)});
 })
